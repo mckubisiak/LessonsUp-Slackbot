efd7ebef4c5e703492b0bba20307bceb78a80caa
Kubi Kubisiak <mckubisiak@gmail.com>
2022-02-07 19:44:47 -0800

    added slack app homepage and hello response to matching channel

--- app.js
+++ app.js
@@ -1,12 +1,89 @@
 require('dotenv').config();
-const { App } = require('@slack/bolt');
+const { App, LogLevel } = require('@slack/bolt');
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  logLevel: LogLevel.DEBUG,
 });
 
 /* Add functionality here */
+app.event('app_home_opened', async ({ event, client, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await client.views.publish({
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "*Welcome to your _LessonUp's Home_* :tada:",
+            },
+          },
+          {
+            type: 'divider',
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app.",
+            },
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  text: 'Click me!',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'Click Me',
+          },
+          action_id: 'button_click',
+        },
+      },
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
+});
 
 (async () => {
   // Start the app
